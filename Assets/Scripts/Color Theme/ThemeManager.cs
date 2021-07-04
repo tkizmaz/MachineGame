99bd4b683f758a920ebf303d2da620df499d1ca0
Yasin Ay <yasiinn@hotmail.com>
2021-07-04 22:46:27 +0300

    Theme Manager
    Theme Manager added
    TextMesh Pro settings done
    Hole sprite tree done
    TODO: UI and sprite animation system with a tween library

---
+++ Assets/Scripts/Color Theme/ThemeManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThemeManager : MonoBehaviour
+{
+    #region Variables
+    public static Action<Color> ThemeChanged;
+    private Color hexColor;
+    #endregion
+
+    #region Unity's functions
+    private void Start()
+    {
+        if (PlayerPrefs.GetString("ThemeColor") != null)
+        {
+            ChangeTheme(PlayerPrefs.GetString("ThemeColor"));
+        }
+    }
+    #endregion
+
+    #region Public functions
+    public void ChangeTheme(string hexCode) 
+    {
+        if (ColorUtility.TryParseHtmlString(hexCode, out hexColor))
+        {
+            ThemeChanged?.Invoke(hexColor);
+            PlayerPrefs.SetString("ThemeColor", hexCode);
+        }
+    }
+    #endregion
+}
